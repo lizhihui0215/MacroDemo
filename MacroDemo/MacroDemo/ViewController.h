e2b96836b5b7f653699520495499afcf61c231bd
李智慧 <lizhihui@lizhihuideMac-mini.local>
2015-09-02 17:24:45 +0800

    add project

---
+++ MacroDemo/MacroDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MacroDemo
+//
+//  Created by 李智慧 on 9/2/15.
+//  Copyright (c) 2015 lizhihui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MacroDemo/MacroDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MacroDemo
+//
+//  Created by 李智慧 on 9/2/15.
+//  Copyright (c) 2015 lizhihui. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
